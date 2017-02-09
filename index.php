a0f7dbba1379a028a46ba83a47b3858ea69855f1
Varun Suresh <varunsureshkumar@g.harvard.edu>
2017-02-09 18:17:54 +0530

    Corrected a spelling error in the description

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
    <img src="images/varun_image.jpg" alt="Varun"/>
    <h2>About Me</h2>
    <p>Hello ! My name is Varun Suresh Kumar and I am currently located in Bangalore City (India).
-   I passed out of high school 2 years ago and I am now an undergraduate student at HES.
-   I am also a national level badminton player training to one day repesent my country at the World Championships and the Olympics.
-   I have taken a couple of programming courses in the past two semesters but all of my web development knowledge is self taught.
+   I passed out of high school two years ago and I am now an undergraduate student at HES.
+   I am also a national level badminton player training to one day represent my country at the World Championships and the Olympics.
+   I have taken a couple of programming courses in the past two semesters but all of my web development skills are self taught.
    I am looking forward to a great semester where I can sharpen my skills in the field of dynamic web applications.</p>
    <h2>Random Quote</h2>
    <p><?php echo $quote; ?></p>
