4da77124fa0848a99192b10ab744a6af5a3b8662
Varun Suresh <varunsureshkumar@g.harvard.edu>
2017-02-09 12:49:36 +0530

    initial commit- Added basic information to the webpage

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+ <head>
+  <title>Varun Suresh Kumar</title>
+  <link rel="stylesheet" href="styles.css">
+ </head>
+ <body>
+  <div>
+   <h1>Varun Suresh Kumar</h1>
+   <img src="images/varun image.jpg"/>
+   <h2>About Me</h2>
+   <p>Hello ! My name is Varun Suresh Kumar and I am currently located in Bangalore City (India).
+   I passed out of high school 2 years ago and I am now an undergraduate student at HES.
+   I am also a national level badminton player training to one day repesent my country at the World Championships and the Olympics.
+   I have taken a couple of programming courses in the past two semesters but all of my web development knowledge is self taught.
+   I am looking forward to a great semester where I can sharpen my skills in the field of dynamic web applications.</p>
+   <h2>Random Quote</h2>
+  </div>
+ </body>
+</html>
